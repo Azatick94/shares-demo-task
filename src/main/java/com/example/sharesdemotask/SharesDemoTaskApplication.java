2823c898283c910fe80f7f8bb0f7203946fc9f77
Azat Burkhanov <azburhanov@mail.ru>
2021-09-16 09:07:54 +0300

    init commit

---
+++ src/main/java/com/example/sharesdemotask/SharesDemoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharesdemotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesDemoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesDemoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharesdemotask/SharesDemoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharesdemotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharesDemoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
